322c31cc9ea0728a0b63a632daa8c2d43c9569c6
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-11-22 00:08:48 +0200

    did 1 task

---
+++ js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
+// Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+function createMarkupGallery (arr) {
+    return arr
+    .map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>`
+    })
+    .join("");
+}; //1+
+
+const marcupGalleryItems = createMarkupGallery(galleryItems);
+galleryEl.innerHTML = marcupGalleryItems;
+
+
+galleryEl.addEventListener(`click`, onGalleryItemClick); //2+
+
+function onGalleryItemClick(e) {
+    const galleryImg = e.target.classList.contains(`gallery__image`);
+    
+    e.preventDefault();
+
+    if(!galleryImg) {
+        return
+    }
+
+    const bigGalleryImg = e.target.dataset.source; //2+
+    // console.log(bigGalleryImg);
+
+    const modal = basicLightbox.create(`
+        <img width="1400" height="900" src="${bigGalleryImg}">
+    `);
+    modal.show();
+    
+    document.addEventListener("keydown", onModalClose);
+
+    function onModalClose(e) {
+        if ("Escape" === e.code) {
+            modal.close();
+        }
+    }
+};
+
